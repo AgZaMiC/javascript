1c76d2f0f1bd270995915fe42d7db33ad7001342
AgZaMiC <profi.seonal102016@gmail.com>
2025-06-26 13:07:19 +0500

    pro

---
+++ index.js
@@ -0,0 +1,13 @@
+alert(4); // number: 1, 10, 100, 1000, -10
+alert("Hello, World!"); // string: "matlar qo'shtirnoq ichida turishi kerak"
+alert(true); // boolean: true va false
+
+alert("typeof -0.48" + typeof "-0.48")
+
+alert(Number("123")); // 123
+alert(String(true)); // "true"
+alert(Boolean(12)); // true
+
+// Data Types: number, string, boolean
+// Commands: alert(), prompt()
+// Types Convertation: typeof
