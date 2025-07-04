71c0594a0c6ff150cc641988876ffdd5a5c7878f
AgZaMiC <profi.seonal102016@gmail.com>
2025-07-04 14:26:56 +0500

    pro

--- index.js
+++ index.js
@@ -1,40 +1,8 @@
-alert(4); // number: 1, 10, 100, 1000, -10
-alert("Hello, World!"); // string: "matlar qo'shtirnoq ichida turishi kerak"
-alert(true); // boolean: true va false
+const first = 10;
+alert(first);
 
-alert("typeof -0.48" + typeof "-0.48");
+const second = 20;
+alert (second);
 
-alert(Number("123")); // 123
-alert(String(true)); // "true"
-alert(Boolean(12)); // true
-
-// Data Types: number, string, boolean
-// Commands: alert(), prompt(), typeof()
-
-
-
-const name = "Saida'zam";
-let age = 9;
-
-// one year later
-age = 10;
-
-alert(name);
-alert(age);
-
-// Arithmetic operators: +, -, *, /, %
-alert(50 * 10);
-alert(500 / 10);
-alert(500 / 50);
-
-const floor = Math.floor(2.1);
-const random = Math.floor(Math.random() * 10);
-const max = Math.max(1, 2, 9, 6);
-
-alert(random);
-alert(max);
-
-
-
-alert(5 * 2);
-alert(95 / 2);
+const third = 30;
+alert(third)
